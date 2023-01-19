8886f5ee8084efce148dc0f76fbcf5636500784e
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-01-20 00:28:42 +0100

    Use models query on the front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
 import { Card } from "../components/Grid/Card";
 import { Grid } from "../components/Grid/Grid";
+import { ModelsQuery } from "../graphql/queries/models";
 import { BaseLayout } from "../layouts/BaseLayout";
 
 export default function Home() {
+    const { data } = useQuery(ModelsQuery);
+    console.log(data)
     return (
         <BaseLayout>
             <Grid>
@@ -19,4 +23,4 @@ export default function Home() {
             </Grid>
         </BaseLayout>
     )
-}
+};
