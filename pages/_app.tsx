3f6ba36f2c76e881e4b5c86a0ef81a04c2732ef7
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-01-20 00:27:40 +0100

    Fix next warning about title tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from "../theme/theme";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-    return <ChakraProvider theme={theme}><Component {...pageProps} /></ChakraProvider>;
+    return (<>
+        <Head>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <title>gcode gizmos</title>
+        </Head>
+        <ApolloProvider client={client}>
+            <ChakraProvider theme={theme}>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </ApolloProvider>
+    </>);
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
         <Html>
-            <Head>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
-                <title>gcode gizmos</title>
-            </Head>
+            <Head />
             <body>
                 <Main />
                 <NextScript />
