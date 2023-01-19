2fe84dbd101d0d23e36e94dc9929d9e13d0fe9df
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-01-19 01:44:36 +0100

    Use new grid and card component in the index view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Card } from "../components/Grid/Card";
+import { Grid } from "../components/Grid/Grid";
+import { BaseLayout } from "../layouts/BaseLayout";
 
 export default function Home() {
-  return (
-    <h1>hello</h1>
-  )
+    return (
+        <BaseLayout>
+            <Grid>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </Grid>
+        </BaseLayout>
+    )
 }
