cd201bfa5b1b7c7527cc42dcf2f3b51506d47cdd
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-01-19 01:43:42 +0100

    Create base layout

---
+++ layouts/BaseLayout.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@chakra-ui/layout";
+import React from "react";
+import { Footer } from "../components/Footer/Footer";
+import { Header } from "../components/Header/Header";
+
+
+export const BaseLayout = ({ children }) => {
+    return (
+        <>
+            <Header />
+            <Box py={4} px={{base: 4, xl: 0}} maxW="960px" as="main" mx="auto" >
+                {children}
+            </Box>
+            <Footer />
+        </>
+    )
+}
