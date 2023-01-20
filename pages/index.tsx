d9c1e8523085bdb489978741341e5d831eb6b614
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-01-20 02:08:37 +0100

    Use api data, add loading spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@apollo/client";
+import { Flex, Spinner } from "@chakra-ui/react";
 import React from "react";
 import { Card } from "../components/Grid/Card";
 import { Grid } from "../components/Grid/Grid";
@@ -6,20 +7,20 @@ import { ModelsQuery } from "../graphql/queries/models";
 import { BaseLayout } from "../layouts/BaseLayout";
 
 export default function Home() {
-    const { data } = useQuery(ModelsQuery);
-    console.log(data)
+    const { data, loading } = useQuery(ModelsQuery);
+    if (loading) {
+        return (
+            <BaseLayout>
+                <Flex align="center" justify="center" w="full" minH="85vh">
+                    <Spinner color='purple.200' size="xl" />
+                </Flex>
+            </BaseLayout>
+        )
+    }
     return (
         <BaseLayout>
             <Grid>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {data?.models.map(model => <Card name={model.name} imageUrl={model.modelImage.url} description={model.description} slug={model.slug} />)}
             </Grid>
         </BaseLayout>
     )
