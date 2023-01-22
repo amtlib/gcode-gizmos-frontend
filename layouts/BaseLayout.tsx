50815974a65333e6301b3b4202a60bf9607c97b2
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-01-22 17:37:34 +0100

    Add rest props support, adjust min height

--- layouts/BaseLayout.tsx
+++ layouts/BaseLayout.tsx
@@ -1,14 +1,14 @@
 import { Box } from "@chakra-ui/layout";
-import React from "react";
+import React, { ComponentPropsWithRef } from "react";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 
 
-export const BaseLayout = ({ children }) => {
+export const BaseLayout = ({ children, ...rest }: {children: React.ReactNode } & ComponentPropsWithRef<typeof Box>) => {
     return (
         <>
             <Header />
-            <Box py={4} px={{base: 4, xlg: 0}} maxW="960px" as="main" mx="auto" >
+            <Box py={4} px={{base: 4, xlg: 0}} maxW="960px" as="main" mx="auto" minH={{ base: "calc(100vh - 165px)", xsm: "calc(100vh - 130px)"}} {...rest}>
                 {children}
             </Box>
             <Footer />
