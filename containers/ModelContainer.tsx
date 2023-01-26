4667175a4d516c5da22d8801d8a3c5722e5de5f6
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-01-26 20:59:57 +0100

    Add missing model fields

--- containers/ModelContainer.tsx
+++ containers/ModelContainer.tsx
@@ -1,22 +1,32 @@
-import { useMutation } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
+import { useContext, useMemo } from "react";
 import { ModelContext } from "../contexts/ModelContext";
-import { CreateModel, DislikeModel, LikeModel } from "../graphql/operations/models";
+import { UserContext } from "../contexts/UserContext";
+import { CreateModel, DeleteModel, ModelsQuery, UpdateModel } from "../graphql/operations/models";
 
 
 export function ModelContainer({ children }) {
-    const [createModelMutation, { data: createModelData, loading: createModelLoading}] = useMutation(CreateModel);
-    const [likeModelMutation, { data: likeModelData, loading: likeModelLoading}] = useMutation(LikeModel);
-    const [dislikeModelMutation, { data: dislikeModelData, loading: dislikeModelLoading}] = useMutation(DislikeModel);
+    const [createModelMutation, { loading: createModelLoading }] = useMutation(CreateModel, { refetchQueries: ["Models"]});
+    const [updateModelMutation, { loading: updateModelLoading }] = useMutation(UpdateModel, { refetchQueries: ["Model"]});
+    const [deleteModelMutation, { loading: deleteModelLoading }] = useMutation(DeleteModel, { refetchQueries: ["Models"]});
+    const {data: dataModels, loading: modelsLoading} = useQuery(ModelsQuery);
+    const { username } = useContext(UserContext);
 
-    const createModel = async (name: string, description: string, modelFile: File, modelImage: File) => {
-        const result = await createModelMutation({ variables: {
-            data: {
-                name,
-                description,
-                modelFile: {upload: modelFile},
-                modelImage: {upload: modelImage}
+    const createModel = async (name: string, description: string, modelFile: File, modelImage: File, recommendedInfill: number, recommendedMaterial: 'pla' | 'abs' | 'pet' | 'tpe', supports: 'yes' | 'no' | 'n/a') => {
+        const result = await createModelMutation({
+            variables: {
+                data: {
+                    name,
+                    description,
+                    modelFile: { upload: modelFile },
+                    modelImage: { upload: modelImage },
+                    createdBy: { connect: { username } },
+                    recommendedInfill,
+                    recommendedMaterial,
+                    supports
+                }
             }
-        }});
+        });
 
         if (result?.data?.createModel?.slug) {
             return result.data.createModel.slug;
@@ -24,31 +34,48 @@ export function ModelContainer({ children }) {
         return null;
     }
 
-    const likeModel = async (modelSlug: string, username: string) => {
-        const result = await likeModelMutation({ variables: {
-            modelSlug,
-            username
-        }});
-        if (result?.data?.updateModel) {
-            return true;
+    const updateModel = async (slug: string, name: string, description: string, recommendedInfill: number, recommendedMaterial: 'pla' | 'abs' | 'pet' | 'tpe', supports: 'yes' | 'no' | 'n/a') => {
+        const result = await updateModelMutation({
+            variables: {
+                slug,
+                data: {
+                    name,
+                    description,
+                    recommendedInfill,
+                    recommendedMaterial,
+                    supports
+                }
+            }
+        });
+
+        if (result?.data?.updateModel?.slug) {
+            return result.data.updateModel.slug;
         }
-        return false;
+        return null;
     }
 
-    const dislikeModel = async (modelSlug: string, username: string) => {
-        const result = await dislikeModelMutation({ variables: {
-            modelSlug,
-            username
+    const deleteModel = async (slug: string) => {
+        const result = await deleteModelMutation({variables: {
+            slug
         }});
-        if (result?.data?.updateModel) {
+
+        if (result.errors) {
+            return false;
+        }
+        if (result.data?.deleteModel) {
             return true;
         }
         return false;
     }
 
     return <ModelContext.Provider value={{
+        models: dataModels?.models || [],
+        modelsLoading,
         createModel,
-        likeModel,
-        dislikeModel
+        updateModel,
+        deleteModel,
+        createModelLoading,
+        deleteModelLoading,
+        updateModelLoading
     }}>{children}</ModelContext.Provider>
 }
--- contexts/ModelContext.ts
+++ contexts/ModelContext.ts
@@ -1,13 +1,36 @@
 import { createContext } from "react";
 
+export type ModelType = {
+    __typename?: "Model";
+    id: string;
+    name?: string;
+    description?: string;
+    slug?: string;
+    doUserLikesIt?: boolean;
+    modelImage?: {
+        __typename?: "ImageFieldOutput";
+        url: string;
+    };
+}
+
 export type ModelContextType = {
-    createModel: (name: string, description: string, modelFile: File, modelImage: File) => Promise<string | null>;
-    likeModel: (modelSlug: string, username: string) => Promise<boolean>;
-    dislikeModel: (modelSlug: string, username: string) => Promise<boolean>;
+    models: ModelType[];
+    modelsLoading: boolean;
+    createModel: (name: string, description: string, modelFile: File, modelImage: File, recommendedInfill: number, recommendedMaterial: 'pla' | 'abs' | 'pet' | 'tpe', supports: 'yes' | 'no' | 'n/a') => Promise<string | null>;
+    updateModel: (slug: string, name: string, description: string, recommendedInfill: number, recommendedMaterial: 'pla' | 'abs' | 'pet' | 'tpe', supports: 'yes' | 'no' | 'n/a') => Promise<string | null>;
+    deleteModel: (slug: string) => Promise<boolean>;
+    createModelLoading: boolean;
+    updateModelLoading: boolean;
+    deleteModelLoading: boolean;
 };
 
 export const ModelContext = createContext<ModelContextType>({
+    models: [],
+    modelsLoading: false,
     createModel: () => Promise.resolve(null),
-    likeModel: () => Promise.resolve(false),
-    dislikeModel: () => Promise.resolve(false),
+    updateModel: () => Promise.resolve(null),
+    deleteModel: () => Promise.resolve(false),
+    createModelLoading: false,
+    updateModelLoading: false,
+    deleteModelLoading: false,
 });
