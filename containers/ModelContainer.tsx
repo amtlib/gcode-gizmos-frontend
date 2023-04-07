1847a1ca5af82dde389f893d81db2c0d6e020689
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-04-07 10:43:25 +0200

    Fix lint

--- containers/ModelContainer.tsx
+++ containers/ModelContainer.tsx
@@ -1,25 +1,26 @@
 import { useMutation, useQuery } from "@apollo/client";
-import { useContext, useMemo } from "react";
+import { useContext } from "react";
 import { ModelContext } from "../contexts/ModelContext";
 import { UserContext } from "../contexts/UserContext";
 import { CreateModel, DeleteModel, ModelsQuery, UpdateModel } from "../graphql/operations/models";
 
 
 export function ModelContainer({ children }) {
-    const [createModelMutation, { loading: createModelLoading }] = useMutation(CreateModel, { refetchQueries: ["Models"]});
-    const [updateModelMutation, { loading: updateModelLoading }] = useMutation(UpdateModel, { refetchQueries: ["Model"]});
-    const [deleteModelMutation, { loading: deleteModelLoading }] = useMutation(DeleteModel, { refetchQueries: ["Models"]});
-    const {data: dataModels, loading: modelsLoading} = useQuery(ModelsQuery);
+    const [createModelMutation, { loading: createModelLoading }] = useMutation(CreateModel, { refetchQueries: ["Models"] });
+    const [updateModelMutation, { loading: updateModelLoading }] = useMutation(UpdateModel, { refetchQueries: ["Model"] });
+    const [deleteModelMutation, { loading: deleteModelLoading }] = useMutation(DeleteModel, { refetchQueries: ["Models"] });
+    const { data: dataModels, loading: modelsLoading } = useQuery(ModelsQuery);
     const { username } = useContext(UserContext);
 
-    const createModel = async (name: string, description: string, modelFile: File, modelImage: File, recommendedInfill: number, recommendedMaterial: 'pla' | 'abs' | 'pet' | 'tpe', supports: 'yes' | 'no' | 'n/a') => {
+    const createModel = async (name: string, description: string, files: File[], images: File[], recommendedInfill: number, recommendedMaterial: 'pla' | 'abs' | 'pet' | 'tpe', supports: 'yes' | 'no' | 'n/a') => {
+        
         const result = await createModelMutation({
             variables: {
                 data: {
                     name,
                     description,
-                    modelFile: { upload: modelFile },
-                    modelImage: { upload: modelImage },
+                    images: { create: [...Array.from(images).map(image => ({ image: { upload: image}, createdBy: {connect: {username}}}))]},
+                    files: { create: [...Array.from(files).map(file => ({ file: { upload: file}, createdBy: {connect: {username}}}))]},
                     createdBy: { connect: { username } },
                     recommendedInfill,
                     recommendedMaterial,
@@ -55,9 +56,11 @@ export function ModelContainer({ children }) {
     }
 
     const deleteModel = async (slug: string) => {
-        const result = await deleteModelMutation({variables: {
-            slug
-        }});
+        const result = await deleteModelMutation({
+            variables: {
+                slug
+            }
+        });
 
         if (result.errors) {
             return false;
