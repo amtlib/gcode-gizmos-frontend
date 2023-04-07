5b82f9bbe3a4972f4de45f45a460180008453961
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-04-07 10:40:34 +0200

    Fix api url

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const PORT = parseInt(process.env.PORT) || 3000;
 
 export const API_URL =
-    process.env.NEXT_PUBLIC_API_URL || `http://localhost:3000/api/graphql`;
+    process.env.NEXT_PUBLIC_API_URL || `http://127.0.0.1:3500/api/graphql`;
