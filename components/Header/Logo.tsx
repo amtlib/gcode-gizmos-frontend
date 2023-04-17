361baf6ccc08dd764415105ad2435d4ccd651333
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-04-18 01:17:33 +0200

    Add test change

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -4,6 +4,6 @@ import { BsBox } from "react-icons/bs";
 
 export const Logo = () => {
     return (
-        <Flex dir='row' gap={3} align="center" mr={2}><Box minW="30px" minH="30px" ml={{ base: 2, xlg: 0 }}><BsBox style={{ width: "100%", height: "100%" }} /></Box> gcode gizmos</Flex>
+        <Flex dir='row' gap={3} align="center" mr={2}><Box minW="30px" minH="30px" ml={{ base: 2, xlg: 0 }}><BsBox style={{ width: "100%", height: "100%" }} /></Box> gcode gizmos test change</Flex>
     )
 }
