35c039f30e0db4518446e3d6604e9d460d51a8c8
amtlib <grzegorz.andrzej.pach@gmail.com>
2023-04-18 01:12:53 +0200

    Add pull to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull
 docker container ls | grep gcode-gizmos-frontend | awk '{print $1}' | xargs docker container kill
-docker system prune
+docker system prune -y
 docker-compose build --no-cache && docker-compose up -d
