0b750d158bb9e8586cbd5baeb9019ec3795e99a1
amtlib <grzegorz.pach@protonmail.com>
2023-04-20 01:34:57 +0200

    Set select borders not rounded

--- theme/theme.tsx
+++ theme/theme.tsx
@@ -32,6 +32,98 @@ export const theme = extendTheme({
                 rounded: 0
             }
         },
+        Select: {
+            sizes: {
+                xs: {
+                    field: {
+                        borderRadius: 0,
+                        fontSize: "xs",
+                        height: 6,
+                        paddingX: 2,
+                    },
+                },
+                sm: {
+                    field: {
+                        borderRadius: 0,
+                        fontSize: "sm",
+                        height: 8,
+                        paddingX: 3,
+                    },
+                },
+                md: {
+                    field: {
+                        borderRadius: 0,
+                        fontSize: "md",
+                        height: 10,
+                        paddingX: 4,
+                    },
+                },
+                lg: {
+                    field: {
+                        borderRadius: 0,
+                        fontSize: "lg",
+                        height: 12,
+                        paddingX: 4,
+                    },
+                },
+            },
+            variants: {
+                outline: {
+                    field: {
+                        background: "inherit",
+                        border: "1px solid",
+                        borderColor: "inherit",
+                        focusBorderColor: "purple",
+                        _focus: {
+                            zIndex: 1,
+                            borderColor: "purple.200",
+                            boxShadow: 0,
+                        },
+                        _hover: { borderColor: "gray.300" },
+                    },
+                },
+                filled: {
+                    field: {
+                        background: "gray.100",
+                        border: "2px solid",
+                        borderColor: "transparent",
+                        _focus: {
+                            background: "transparent",
+                            borderColor: "purple.200",
+                        },
+                        _hover: {
+                            background: "gray.300",
+                        },
+                    },
+                },
+                flushed: {
+                    field: {
+                        background: "transparent",
+                        borderBottom: "1px solid",
+                        borderColor: "inherit",
+                        borderRadius: 0,
+                        paddingX: 0,
+                        _focus: {
+                            borderColor: "purple.200",
+                            boxShadow: 0,
+                        },
+                    },
+                },
+                unstyled: {
+                    field: {
+                        background: "transparent",
+                        borderRadius: 0,
+                        height: "auto",
+                        paddingX: 0,
+                    },
+                },
+            },
+            defaultProps: {
+                size: "md",
+                variant: "outline",
+                focusBorderColor: "purple"
+            },
+        },
         Input: {
             sizes: {
                 xs: {
