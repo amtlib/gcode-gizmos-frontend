d0459db1adf780b1ab71678d7243f1e8e4d7b1b0
amtlib <grzegorz.pach@protonmail.com>
2023-04-25 23:53:56 +0200

    Send comment to the api

--- components/Comments/AddComment/AddComment.tsx
+++ components/Comments/AddComment/AddComment.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { RichTextBlock } from "./RichTextBlocks";
 import { Button, Flex } from "@chakra-ui/react";
 import { Descendant } from "slate";
+import { ModelContext } from "../../../contexts/ModelContext";
+import { UserContext } from "../../../contexts/UserContext";
 
 const initialValue = [
     {
@@ -10,12 +12,22 @@ const initialValue = [
     }
 ];
 
-export const AddComment = () => {
+export const AddComment = ({ modelSlug }: {modelSlug: string }) => {
+    const { loggedIn } = useContext(UserContext);
     const [value, setValue] = useState<Descendant[]>(initialValue);
+    const { createComment} = useContext(ModelContext);
+
+    if(!loggedIn) {
+        return null;
+    }
+
+    const handleSend = () => {
+        createComment(modelSlug, value);
+    }
     return (
         <Flex direction="column" gap="3">
             <RichTextBlock value={value} setValue={setValue}/>
-            <Button alignSelf="flex-end">Add comment</Button>
+            <Button alignSelf="flex-end" onClick={handleSend}>Add comment</Button>
         </Flex>
     )
 }
