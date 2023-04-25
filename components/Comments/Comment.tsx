913e80e4463cf94f0fa0bb9b9c808361a623a23c
amtlib <grzegorz.pach@protonmail.com>
2023-04-26 01:06:05 +0200

    Adjust Comment spacing

--- components/Comments/Comment.tsx
+++ components/Comments/Comment.tsx
@@ -6,17 +6,17 @@ import React from "react";
 export const Comment = ({ document, username, createdAt }: { username: string; document: any, createdAt: string }) => {
     const formattedDate = format(Date.parse(createdAt), 'dd/LL/yyyy HH:mm:ss');
     return (
-        <Flex alignItems="flex-start" gap={3} _dark={{ bg: "gray.700"}} bg="gray.100" position="relative">
+        <Flex alignItems="center" gap={3} _dark={{ bg: "gray.700"}} bg="gray.100" position="relative">
             <Tooltip label={username}>
                 <Avatar
                     my={8}
                     mx={3}
-                    alignSelf="flex-start"
+                    mr={1}
                     size={'sm'}
                     name={username}
                 />
             </Tooltip>
-            <Box px={3} py={8} w="full" alignSelf="flex-start">
+            <Box px={3} pl={1} py={8} w="full">
                 <DocumentRenderer document={document} />
             </Box>
             <Box _dark={{ color: "gray.300"}} color="gray.500" p={1} position="absolute" right="0" alignSelf="flex-end" justifySelf="flex-end" textAlign="right">{formattedDate}</Box>
