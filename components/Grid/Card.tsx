46923321dccde9845a08d7056470a601d9963441
amtlib <grzegorz.pach@protonmail.com>
2023-05-20 12:33:36 +0200

    Expand link to the whole card

--- components/Grid/Card.tsx
+++ components/Grid/Card.tsx
@@ -34,50 +34,40 @@ export const Card = React.memo(function CardComponent({ name, imageUrl, slug, do
     }
 
     return (
-        <GridItem>
-            <Box
-                my={5}
-                mx={0}
-                overflow={'hidden'}
-                bg="white"
-                border={'1px'}
-                borderColor="black"
-                boxShadow={useColorModeValue(`6px 6px 0 ${shadowWhite}`, `6px 6px 0 ${shadowBlack}`)}>
-                <Box borderBottom={'1px'} borderColor="black">
-                    <Image
-                        src={
-                            imageUrl
-                        }
-                        objectFit="cover"
-                        h="full"
-                        w="full"
-                        alt=""
-                        fallback={<>
-                            <Flex w="full" h="full" align="center" justify="center">
-                                <BsBox style={{ width: "80%", height: "80%" }} color="black" />
-                            </Flex>
-                        </>}
-                    />
-                </Box>
-                <Box p={4}>
-                    {/* <Box
-                        bg="black"
-                        display={'inline-block'}
-                        px={2}
-                        py={1}
-                        color="white"
-                        mb={2}>
-                        <Text fontSize={'xs'} fontWeight="medium">
-                            React
-                        </Text>
-                    </Box> */}
-                    <Heading color={'black'} fontSize={'2xl'} noOfLines={3}>
-                        {name}
-                    </Heading>
-                </Box>
-                <HStack borderTop={'1px'} color="black">
-                    <Box p={4} w="full">
-                        <Link href={`/model/${slug}`}>
+        <Link href={`/model/${slug}`}>
+            <GridItem>
+                <Box
+                    my={5}
+                    mx={0}
+                    overflow={'hidden'}
+                    bg="white"
+                    border={'1px'}
+                    borderColor="black"
+                    boxShadow={useColorModeValue(`6px 6px 0 ${shadowWhite}`, `6px 6px 0 ${shadowBlack}`)}>
+                    <Box borderBottom={'1px'} borderColor="black">
+                        <Image
+                            src={
+                                imageUrl
+                            }
+                            objectFit="cover"
+                            h="full"
+                            w="full"
+                            alt=""
+                            fallback={<>
+                                <Flex w="full" h="full" align="center" justify="center">
+                                    <BsBox style={{ width: "80%", height: "80%" }} color="black" />
+                                </Flex>
+                            </>}
+                        />
+                    </Box>
+                    <Box p={4}>
+                        <Heading color={'black'} fontSize={'2xl'} noOfLines={3}>
+                            {name}
+                        </Heading>
+                    </Box>
+                    <HStack borderTop={'1px'} color="black">
+                        <Box p={4} w="full">
+
                             <Flex
                                 alignItems="center"
                                 justifyContent={'space-between'}
@@ -89,26 +79,30 @@ export const Card = React.memo(function CardComponent({ name, imageUrl, slug, do
                                 </Text>
                                 <BsArrowUpRight />
                             </Flex>
-                        </Link>
-                    </Box>
-                    <Tooltip label={loggedIn ? liked ? 'Click to dislike!' : 'Click to like!' : 'Log in to like models!'}>
-                        <Flex
-                            p={4}
-                            alignItems="center"
-                            justifyContent={'space-between'}
-                            roundedBottom={'sm'}
-                            borderLeft={'1px'}
-                            cursor="pointer"
-                            onClick={() => loggedIn ? handleLike() : null}>
-                            {liked ? (
-                                <BsHeartFill fill="red" fontSize={'24px'} />
-                            ) : (
-                                <BsHeart fontSize={'24px'} />
-                            )}
-                        </Flex>
-                    </Tooltip>
-                </HStack>
-            </Box>
-        </GridItem>
+                        </Box>
+                        <Tooltip label={loggedIn ? liked ? 'Click to dislike!' : 'Click to like!' : 'Log in to like models!'}>
+                            <Flex
+                                p={4}
+                                alignItems="center"
+                                justifyContent={'space-between'}
+                                roundedBottom={'sm'}
+                                borderLeft={'1px'}
+                                cursor="pointer"
+                                zIndex={2}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    if (loggedIn) handleLike();
+                                }}>
+                                {liked ? (
+                                    <BsHeartFill fill="red" fontSize={'24px'} />
+                                ) : (
+                                    <BsHeart fontSize={'24px'} />
+                                )}
+                            </Flex>
+                        </Tooltip>
+                    </HStack>
+                </Box>
+            </GridItem>
+        </Link>
     );
 })
