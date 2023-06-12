85bf1a015cf37f8ee9e258c9fb10436b346a1d88
amtlib <grzegorz.pach@protonmail.com>
2023-06-13 00:07:19 +0200

    Add error handling

--- components/Comments/AddComment/AddComment.tsx
+++ components/Comments/AddComment/AddComment.tsx
@@ -4,6 +4,7 @@ import { Button, Flex } from "@chakra-ui/react";
 import { Descendant } from "slate";
 import { ModelContext } from "../../../contexts/ModelContext";
 import { UserContext } from "../../../contexts/UserContext";
+import { Text } from "@chakra-ui/react";
 
 const initialValue = [
     {
@@ -15,18 +16,27 @@ const initialValue = [
 export const AddComment = ({ modelSlug }: {modelSlug: string }) => {
     const { loggedIn } = useContext(UserContext);
     const [value, setValue] = useState<Descendant[]>(initialValue);
+    const [hasErrors, setHasErrors] = useState<boolean>(false);
     const { createComment} = useContext(ModelContext);
 
     if(!loggedIn) {
         return null;
     }
-
     const handleSend = () => {
-        createComment(modelSlug, value);
+        // @ts-ignore
+        if (value.length < 2 && value?.[0]?.children?.[0].text === "") {
+            setHasErrors(true);
+        } else {
+            createComment(modelSlug, value);
+            setValue(initialValue);
+            setHasErrors(false);
+        }
     }
+
     return (
         <Flex direction="column" gap="3">
             <RichTextBlock value={value} setValue={setValue}/>
+            {hasErrors && <Text color="red.500">Please provide any content before adding the comment</Text>}
             <Button alignSelf="flex-end" onClick={handleSend}>Add comment</Button>
         </Flex>
     )
